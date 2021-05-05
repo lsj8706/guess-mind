ea4fa11d5000a10f843d0d677b885052c05c50f3
Lee SeJin <lsj8706@naver.com>
2021-05-06 00:29:15 +0900

    socket io

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 import path from "path";
 import express from "express";
 import socketIO from "socket.io";
+import looger from "morgan";
 
 const PORT = 5000;
 const app = express();
 app.set("view engine","pug");
 app.set("views", path.join(__dirname, "views"));
+app.use(looger("dev"));
 app.use(express.static(path.join(__dirname, "static")));
 app.get("/", (req,res) => res.render("home"));
 
@@ -16,3 +18,15 @@ const handleListening = () => console.log(`✅ Server running : http://localhost
 const server = app.listen(PORT, handleListening);
 
 const io = socketIO(server);
+
+
+
+io.on("connection", (socket)=>{
+    socket.on("newMessage", ({message})=>{
+        socket.broadcast.emit("messageNotif",{ message, nickname:socket.nickname || "Anon" });
+    });
+    socket.on("setNickname",({nickname})=>{
+        socket.nickname = nickname;
+    });
+});
+
